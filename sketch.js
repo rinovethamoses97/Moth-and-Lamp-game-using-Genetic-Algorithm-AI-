84e22b6bc5bc61a98045fd77befbfed57e033d29
rino <rinovethamoses97@gmail.com>
2019-02-01 08:03:37 +0530

    Manhattan Distance function added

--- sketch.js
+++ sketch.js
@@ -49,6 +49,9 @@ function evaluate(){
 	}
 	return false;
 }
+function manhattan(x1,y1,x2,y2){
+	return (abs(x2-x1)+abs(y2-y1));
+}
 function calculateFitness(){
 	for(var i=0;i<populationSize;i++){
 		if(population[i].deadbyobstacle){
